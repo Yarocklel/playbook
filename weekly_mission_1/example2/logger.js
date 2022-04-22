1b661161841ed51d5c61082b9a5e918cee62c5bf
Yael Soto <xyarock@gmail.com>
2022-04-22 00:31:53 -0500

    se termino ejercicio 2 semana 1

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,12 @@
+
+
+// esta es una funcion que se guardara en este modulo como info
+exports.info=(message)=>{
+    console.log(`info: ${message}`);
+}
+
+//esta es una funcion que se guardara en este modulo como verbose
+
+exports.verbose=(message)=>{
+    console.log(`verbose: ${message}`);//para concatenar se usa `
+}
---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,5 @@
+const logger=require('./logger');//importa el logger.js
+
+logger.info('this is an informational message');
+
+logger.verbose('this is a verbose message');
